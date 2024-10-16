567bf2608311e232f26d13ddfce76dc8a8208b37
at-sso <therealxink@gmail.com>
2024-10-16 02:14:15 -0400

    added tools

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import ctypes
-from . import globales
-from . import logger
---
+++ src/tools.py
@@ -0,0 +1,79 @@
+__all__ = ["function_handler", "clear_terminal"]
+
+import os
+import time
+import traceback
+
+from .ctypes import *
+from .logger import *
+
+
+def clear_terminal() -> int:
+    """clears the terminal screen using platform-dependent commands."""
+    return os.system("cls" if os.name == "nt" else "clear")
+
+
+def function_handler(func: GenericCallable) -> Any:
+    """Wraps a function call with logging and exception handling.
+
+    This function takes another function (`func`) as an argument and executes it
+    within a monitored context. Here's what happens:
+
+    - Start Time Logging:
+        - Captures the current time.
+        - Logs a message using indicating the start of the wrapped function with its
+          details obtained.
+
+    - Function Execution:
+        - Attempts to call the provided function `func` and stores the return value
+          in `func_val`.
+
+    - Exception Handling:
+        - If an exception occurs during `func` execution:
+            - Formats a final message using.
+            - Logs a critical message using with details about the exception,
+            including the function information and the traceback.
+            - Calls `THE_MAIN_LOOP_WAS_TERMINATED`.
+            - Re-raises the exception to propagate it further.
+
+    - Normal Execution:
+        - If no exception occurs:
+            - Formats a final message.
+
+    - Return Value:
+        - Returns the value returned by the wrapped function.
+    """
+    start: float = time.time()
+
+    def __format_final_message(
+        start: float, func: GenericCallable, is_exception: bool = False
+    ) -> None:
+        """
+        The function `__format_final_message` logs the execution time of a given function based on the start and
+        end timestamps.
+
+        @param start The `start` parameter is the start timestamp of the timer, indicating when the timer
+        was initially started.
+        @param func The `func` parameter in the `__format_final_message` function is a callable that represents
+        the function being executed. It can be any function that can be called with any number of arguments
+        and returns a value of any type.
+        """
+        end: float = time.time()
+        logger.debug(
+            f"{'Unhandled operation' if is_exception else 'Operation'}: "
+            f"{func} took: {abs(start - end)} ms."
+        )
+
+    logger.info(f"Start of: {func}.")
+
+    try:
+        func_val: Any = func()
+    except Exception:
+        __format_final_message(start, func, is_exception=True)
+        logger.critical(
+            f"Unhandled exception raised in {func}:" f"\n{traceback.format_exc()}"
+        )
+        raise
+
+    __format_final_message(start, func)
+    return func_val
